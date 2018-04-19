b67cf1597302dcb38a6952d118a8ce3035cb2188
katharinaeisenhardt <katharina.eisenhardt@hs-furtwangen.de>
2018-04-19 20:34:44 +0200

    updates A3

--- A3/Program.cs
+++ A3/Program.cs
@@ -35,6 +35,7 @@ namespace A3
             int divider;
             systemnumber = 0;
             List<int> residuals = new List<int>();
+
             if(0 <= dec && dec <= 1023){
                 do{
                     int modulo = dec % toBase;
@@ -44,31 +45,36 @@ namespace A3
                     dec = divider;
                 }while(divider != 0);
             }
+
             residuals.Reverse();
             for(int i = 0; i<=residuals.Count-1; i++){
                 systemnumber += residuals[i] * Convert.ToInt32(Math.Pow(10,residuals.Count-i-1));
             }
+
             return systemnumber;
-        
         }
+
         public static int ConvertToDecimalFromBase(int fromBase, int number){
             sum = 0;
             int[] digits = new int[1 + (int)Math.Log10(number)];
-                for(int i = digits.Length-1; i >= 0; i--){
-                    int digit;
-                    number = Math.DivRem(number, 10, out digit);
-                    digits[i]=digit;
-                }
-                List<int> outputs = new List<int>();
-                Array.Reverse(digits);
-                for(int i= 0; i <= digits.Length-1; i++){
-                    int multiplication = digits[i] * (int)Math.Pow(fromBase,i);
-                    outputs.Add(multiplication);
-                }
-                for(int i= 0; i <= outputs.Count-1; i++){
-                    sum = sum + outputs[i];
-                }
-                return sum;
+
+            for(int i = digits.Length-1; i >= 0; i--){
+                int digit;
+                number = Math.DivRem(number, 10, out digit);
+                digits[i]=digit;
+            }
+
+            List<int> outputs = new List<int>();
+            Array.Reverse(digits);
+            for(int i= 0; i <= digits.Length-1; i++){
+                int multiplication = digits[i] * (int)Math.Pow(fromBase,i);
+                outputs.Add(multiplication);
+            }
+
+            for(int i= 0; i <= outputs.Count-1; i++){
+                sum = sum + outputs[i];
+            }
+            return sum;
         }
 
         static int ConvertNumberToBaseFromBase(int number, int toBase, int fromBase){
@@ -80,8 +86,7 @@ namespace A3
             if(2 <= toBase && 10 >= toBase && fromBase <= 10 && fromBase >=2){
                 int dec=0;
                 dec = (ConvertToDecimalFromBase(fromBase,number));
-                systemnumber = ConvertToBaseFromDecimal(toBase,dec);
-                
+                systemnumber = ConvertToBaseFromDecimal(toBase,dec); 
             }
             return systemnumber;
         }
