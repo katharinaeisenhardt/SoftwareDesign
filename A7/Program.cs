5180e6cee46b301d70a4a870c4a72e3ee664f551
katharinaeisenhardt <katharina.eisenhardt@hs-furtwangen.de>
2018-05-31 09:48:20 +0200

    adds A7 - Aufgaben 7

---
+++ A7/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace A7
+{
+
+    public class Person
+    {
+        public string Name;
+        public int Age;
+        private int Id; //= IDGenerator.GetInstance().GibMirNeId();
+
+        /*oder
+        public Person(){
+            this.Id = IDGenerator.GetInstance().GibMirNeId();
+        }*/
+
+        //Jonas' Version
+        public Person(string name, int age){
+            this.Name = name;
+            this.Age = age;
+            this.Id = IDGenerator.Instance.GibMirNeId();
+            Program.personen.Add(this);
+        }
+
+        public override string ToString()
+        {
+            return "Name:" + Name + ", Age: " + Age + ", " + "Id: " + Id;
+        }
+    }
+
+    public class IDGenerator
+    {
+        private IDGenerator()
+        {
+            letzteID = 1;
+        }
+
+        private static IDGenerator _instance;
+
+        public static IDGenerator Instance{
+            get{
+                if (_instance == null)
+                    _instance = new IDGenerator();
+                return _instance;
+            }
+        }
+
+        private int letzteID;
+        public int GibMirNeId()
+        {
+            return letzteID++;
+        }
+    } 
+
+    class Program
+    {
+        public static List<Person> personen = new List<Person>();
+
+        static void Main(string[] args)
+        {
+
+            // Eine Stelle, an der Personen angelegt werden
+            new Person("Dieter", 44);
+            new Person("Horst", 45);
+            new Person("Walter", 33);
+            new Person("Karl-Heinz", 22);
+            //personen.Add(new Person{ Name="Karl-Heinz", Age=22});
+
+
+            // Eine ANDERE Stelle, an der Personen angelegt werden
+            new Person("Brunhilde", 56);
+            new Person("Maria", 75);
+            new Person("Kunigunde", 22);
+            new Person("Tusnelda",12);
+
+            foreach (var person in personen)
+                Console.WriteLine(person);
+
+            //Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ A7Decorator/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace A7Decorator
+{
+    public class Spielfigur
+    {
+        public virtual void Drohe(){
+
+        }  
+    }
+
+    public class Monster : Spielfigur
+    {
+        public override void Drohe()
+        {
+            Console.Write("Grrrrr!");
+        }
+    } 
+
+    class Held : Spielfigur 
+    {
+        public override void Drohe()
+        {
+            Console.Write("Weiche zurück!");
+        }        
+    }
+
+    public class Decorator : Spielfigur
+    {
+        private Spielfigur _original;
+
+         public Decorator(Spielfigur original)
+        {
+            _original = original; 
+        }
+        
+        public override void Drohe(){
+            _original.Drohe();
+        }  
+    }
+
+    // Decorator
+    class ErkaelteteFigur : Decorator
+    {
+        public ErkaelteteFigur(Spielfigur original)
+        : base (original){}
+
+        public override void Drohe()
+        {
+            base.Drohe();
+            Console.Write(" Hust!");
+        }
+    }
+
+    class HeisereFigur : Decorator
+    {
+        public HeisereFigur(Spielfigur original)
+        : base (original){}
+        
+        public override void Drohe()
+        {
+            Console.Write("Räusper...");
+            base.Drohe();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Spielfigur> figuren = new List<Spielfigur>();
+
+            figuren.Add(new Monster());
+            figuren.Add(new Held());
+            figuren.Add(new ErkaelteteFigur(new Held()));
+            figuren.Add(new ErkaelteteFigur(new ErkaelteteFigur(new Monster())));
+            figuren.Add(new HeisereFigur(new Monster()));
+            figuren.Add(new ErkaelteteFigur(new HeisereFigur(new Held())));
+
+            foreach(var spielfigur in figuren)
+            {
+                spielfigur.Drohe();
+                Console.WriteLine();
+            }
+        }
+    }
+}
